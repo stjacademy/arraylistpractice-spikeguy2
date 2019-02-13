6e6d8f0ecad9585acb3516f9323e9e6a699285eb
spikeguy2 <sungwoo.jang@stjlabs.com>
2019-02-13 12:35:46 -0500

    done arraylist factor thing

--- practicearray/src/practicearray/minilabs.java
+++ practicearray/src/practicearray/minilabs.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 public class minilabs {
 	public static void main(String[]args)
 	{
-		System.out.println(getFactor(36));
+		System.out.println(getListOfFactors(36));
+		System.out.println(keepOnlyCompositeNumbers(getListOfFactors(36)));
 	}
 	
-	public static ArrayList getFactor(int num)
+	public static ArrayList getListOfFactors(int num)
 	{
 		ArrayList<Integer> factors = new ArrayList<Integer>();
 		
@@ -20,8 +21,29 @@ public class minilabs {
 		return factors;
 	}
 	
-	public static void removePrime(ArrayList factors)
+	public static ArrayList keepOnlyCompositeNumbers(ArrayList<Integer> factors)
 	{
+		int count=0;
+		
+		for(int i=0; i< factors.size(); i++)
+		{
+			for(int j=2; j<factors.get(i); j++)
+			{
+				if(factors.get(i)%j==0)
+					count++;
+			}
+			
+			if(count==0 || factors.get(i)==2)
+			{
+				factors.remove(i);
+				i--;
+			}
+			
+			count=0;
+		}
+		return factors;
+		
+		
 		
 	}
 	
